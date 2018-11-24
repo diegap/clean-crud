a853c5d924c19785d5bcb9f13a9c1e00f5f75385
diegap <diego.perez@fravega.com.ar>
2018-11-24 19:18:50 -0300

    Initial commit

---
+++ src/main/kotlin/io/clean/crud/CrudApplication.kt
@@ -0,0 +1,11 @@
+package io.clean.crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudApplication>(*args)
+}
