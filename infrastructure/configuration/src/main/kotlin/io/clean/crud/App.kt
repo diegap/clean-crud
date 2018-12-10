ddd38ea0e6eff9dbb7250344d2f1b6ebcebc0142
diegap <diego.perez@fravega.com.ar>
2018-12-10 00:11:24 -0300

    Cleanup main class

--- infrastructure/configuration/src/main/kotlin/io/clean/crud/App.kt
+++ infrastructure/configuration/src/main/kotlin/io/clean/crud/App.kt
@@ -1,16 +1,10 @@
 package io.clean.crud
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
-@SpringBootApplication(exclude = [MongoAutoConfiguration::class, MongoDataAutoConfiguration::class])
-@EnableReactiveMongoRepositories
-@AutoConfigureAfter(EmbeddedMongoAutoConfiguration::class)
+@SpringBootApplication
 class App
 
 fun main(args: Array<String>) {
